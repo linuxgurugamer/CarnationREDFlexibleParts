78cebfc6d9e43b2b1893c984bdea2e2fa5d701fb
Jonathan Bayer <linuxgurugamer@gmail.com>
2021-10-25 14:53:40 -0400

    Language support

--- Main/FARAPI.cs
+++ Main/FARAPI.cs
@@ -11,7 +11,7 @@ namespace CarnationVariableSectionPart
         {
             //EditorGUI.RequestUpdateVoxel();
             var far =  cvsp.part.FindModuleImplementing<GeometryPartModule>();
-            if (!far) return false;
+            if (far == null) return false;
             if (RebuildAllMeshData == null)
                 RebuildAllMeshData = typeof(GeometryPartModule).GetMethod("RebuildAllMeshData", BindingFlags.Instance | BindingFlags.NonPublic);
             RebuildAllMeshData.Invoke(far, null);
